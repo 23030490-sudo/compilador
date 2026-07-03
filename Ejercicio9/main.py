74694f0df1a6a3c81b3f72ab5432b42bebe68fa5
sofi <23030149@itcelaya.edu.mx>
2026-07-03 09:58:01 -0600

    ejercicio9 con archivo txt y capturas correspondientes

--- Ejercicio9/main.py
+++ Ejercicio9/main.py
@@ -1,8 +1,18 @@
 from antlr4 import *
 from ExprLexer import ExprLexer
 
-lexer = ExprLexer(InputStream(input("? ")))
+# Número de control: 23030149
 
+modo = input("¿Leer desde archivo o terminal? (a = archivo, t = terminal): ")
+
+if modo == "a":
+    nombre_archivo = input("Nombre del archivo (ej: entrada.txt): ")
+    input_stream = FileStream(nombre_archivo, encoding='utf-8')
+else:
+    texto = input("? ")
+    input_stream = InputStream(texto)
+
+lexer = ExprLexer(input_stream)
 tokens = CommonTokenStream(lexer)
 tokens.fill()
 
