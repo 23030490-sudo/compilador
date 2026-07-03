77dfa580558c40206c3b158f0afdb36ccc051a28
CarmenArreguin <car252004@gmail.com>
2026-07-03 01:05:24 -0600

    Ejercicio 6 corregido

--- Ejercicio6/Ejercicio6.py
+++ Ejercicio6/Ejercicio6.py
@@ -2,15 +2,34 @@ from antlr4 import *
 from ExprLexer import ExprLexer
 from antlr4.Token import Token
 
-lexer = ExprLexer(InputStream(input("? ")))
+def analizar(texto):
+    lexer = ExprLexer(InputStream(texto))
+    tokens = CommonTokenStream(lexer)
+    tokens.fill()
 
-tokens = CommonTokenStream(lexer)
-tokens.fill()
+    print("LEXEMA\t\tTOKEN")
+    print("-------------------------")
 
-for token in tokens.tokens:
-    if token.type == Token.EOF:
-        continue
+    for token in tokens.tokens:
+        if token.type == Token.EOF:
+            continue
 
-    print("Texto:", token.text)
-    print("Tipo:", lexer.symbolicNames[token.type])
-    print("--------------------")
+        nombre_token = lexer.symbolicNames[token.type]
+        print(f"{token.text}\t\t{nombre_token}")
+
+
+print("1. Entrada desde terminal")
+print("2. Entrada desde archivo")
+opcion = input("Selecciona una opción: ")
+
+if opcion == "1":
+    texto = input("Escribe la entrada: ")
+    analizar(texto)
+
+elif opcion == "2":
+    with open("entrada.txt", "r", encoding="utf-8") as archivo:
+        texto = archivo.read()
+    analizar(texto)
+
+else:
+    print("Opción no válida")
