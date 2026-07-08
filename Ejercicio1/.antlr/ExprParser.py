de9e00760298a757d29bfc86cf4b7a070e7b68d3
23030490-sudo <23030490@itcelaya.edu.mx>
2026-07-08 09:59:04 -0600

    Ejercicio 1: Se sube el ejercicio terminado con ANTLR con las reglas semánticas definidas en Expr.g4 y con el archvio Test.py

--- Ejercicio1/.antlr/ExprParser.py
+++ Ejercicio1/.antlr/ExprParser.py
@@ -10,9 +10,12 @@ else:
 
 def serializedATN():
     return [
-        4,1,3,10,2,0,7,0,2,1,7,1,1,0,1,0,1,0,1,1,1,1,1,1,0,0,2,0,2,0,0,7,
-        0,4,1,0,0,0,2,7,1,0,0,0,4,5,3,2,1,0,5,6,5,0,0,1,6,1,1,0,0,0,7,8,
-        5,0,0,1,8,3,1,0,0,0,0
+        4,1,3,19,2,0,7,0,2,1,7,1,1,0,1,0,1,0,1,1,1,1,1,1,1,1,1,1,1,1,5,1,
+        14,8,1,10,1,12,1,17,9,1,1,1,0,1,2,2,0,2,0,0,17,0,4,1,0,0,0,2,7,1,
+        0,0,0,4,5,3,2,1,0,5,6,5,0,0,1,6,1,1,0,0,0,7,8,6,1,-1,0,8,9,5,2,0,
+        0,9,15,1,0,0,0,10,11,10,2,0,0,11,12,5,1,0,0,12,14,3,2,1,3,13,10,
+        1,0,0,0,14,17,1,0,0,0,15,13,1,0,0,0,15,16,1,0,0,0,16,3,1,0,0,0,17,
+        15,1,0,0,0,1,15
     ]
 
 class ExprParser ( Parser ):
@@ -75,7 +78,7 @@ class ExprParser ( Parser ):
         try:
             self.enterOuterAlt(localctx, 1)
             self.state = 4
-            self.expr()
+            self.expr(0)
             self.state = 5
             self.match(ExprParser.EOF)
         except RecognitionException as re:
@@ -94,31 +97,82 @@ class ExprParser ( Parser ):
             super().__init__(parent, invokingState)
             self.parser = parser
 
-        def EOF(self):
-            return self.getToken(ExprParser.EOF, 0)
+        def NUM(self):
+            return self.getToken(ExprParser.NUM, 0)
+
+        def expr(self, i:int=None):
+            if i is None:
+                return self.getTypedRuleContexts(ExprParser.ExprContext)
+            else:
+                return self.getTypedRuleContext(ExprParser.ExprContext,i)
+
+
+        def MAS(self):
+            return self.getToken(ExprParser.MAS, 0)
 
         def getRuleIndex(self):
             return ExprParser.RULE_expr
 
 
 
-
-    def expr(self):
-
-        localctx = ExprParser.ExprContext(self, self._ctx, self.state)
-        self.enterRule(localctx, 2, self.RULE_expr)
+    def expr(self, _p:int=0):
+        _parentctx = self._ctx
+        _parentState = self.state
+        localctx = ExprParser.ExprContext(self, self._ctx, _parentState)
+        _prevctx = localctx
+        _startState = 2
+        self.enterRecursionRule(localctx, 2, self.RULE_expr, _p)
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 7
-            self.match(ExprParser.EOF)
+            self.state = 8
+            self.match(ExprParser.NUM)
+            self._ctx.stop = self._input.LT(-1)
+            self.state = 15
+            self._errHandler.sync(self)
+            _alt = self._interp.adaptivePredict(self._input,0,self._ctx)
+            while _alt!=2 and _alt!=ATN.INVALID_ALT_NUMBER:
+                if _alt==1:
+                    if self._parseListeners is not None:
+                        self.triggerExitRuleEvent()
+                    _prevctx = localctx
+                    localctx = ExprParser.ExprContext(self, _parentctx, _parentState)
+                    self.pushNewRecursionContext(localctx, _startState, self.RULE_expr)
+                    self.state = 10
+                    if not self.precpred(self._ctx, 2):
+                        from antlr4.error.Errors import FailedPredicateException
+                        raise FailedPredicateException(self, "self.precpred(self._ctx, 2)")
+                    self.state = 11
+                    self.match(ExprParser.MAS)
+                    self.state = 12
+                    self.expr(3) 
+                self.state = 17
+                self._errHandler.sync(self)
+                _alt = self._interp.adaptivePredict(self._input,0,self._ctx)
+
         except RecognitionException as re:
             localctx.exception = re
             self._errHandler.reportError(self, re)
             self._errHandler.recover(self, re)
         finally:
-            self.exitRule()
+            self.unrollRecursionContexts(_parentctx)
         return localctx
 
+
+
+    def sempred(self, localctx:RuleContext, ruleIndex:int, predIndex:int):
+        if self._predicates == None:
+            self._predicates = dict()
+        self._predicates[1] = self.expr_sempred
+        pred = self._predicates.get(ruleIndex, None)
+        if pred is None:
+            raise Exception("No predicate with index:" + str(ruleIndex))
+        else:
+            return pred(localctx, predIndex)
+
+    def expr_sempred(self, localctx:ExprContext, predIndex:int):
+            if predIndex == 0:
+                return self.precpred(self._ctx, 2)
+         
 
 
 
